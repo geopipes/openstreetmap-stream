055491532f7de83256dcf9db3d07ab846b95b014
Peter Johnson <insomnia@rcpt.at>
2014-09-24 15:07:19 +0100

    change pbf parser; increase the awesome; update readme; resolves #1

--- example/stdin.js
+++ example/stdin.js
@@ -2,6 +2,7 @@
 var osm = require('../');
 
 // wget https://s3.amazonaws.com/metro-extracts.mapzen.com/auckland_new-zealand.osm.pbf
-osm.createReadStream( './auckland_new-zealand.osm.pbf' )
+process.stdin
+  .pipe( osm.parser() )
   .pipe( osm.stringify )
   .pipe( process.stdout );
--- test/interface.js
+++ test/interface.js
@@ -9,16 +9,23 @@ module.exports.interface.createReadStream = function(test, common) {
     t.equal(osm.createReadStream.length, 1, 'consistent arguments length');
     t.end();
   });
-}
+};
 
 module.exports.interface.stringify = function(test, common) {
   test('stringify()', function(t) {
-    t.equal(typeof osm.stringify, 'object', 'valid function');
+    t.equal(typeof osm.stringify, 'object', 'valid stream');
     t.equal(typeof osm.stringify._read, 'function', 'readable stream');
     t.equal(typeof osm.stringify._write, 'function', 'writable stream');
     t.end();
   });
-}
+};
+
+module.exports.interface.parser = function(test, common) {
+  test('parser()', function(t) {
+    t.equal(typeof osm.parser, 'function', 'valid function');
+    t.end();
+  });
+};
 
 module.exports.all = function (tape, common) {
 
